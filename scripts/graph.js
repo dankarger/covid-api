319188b4888f35659def13921b8a5832a759ad8a
dankarger <karger.dan@gmail.com>
2021-12-09 18:18:25 +0200

    starting testing with graphs

---
+++ scripts/graph.js
@@ -0,0 +1,6 @@
+// import {dataCollected} from "./main.js";
+
+export function printData(data) {
+    console.log('print',data)
+    return data
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,22 @@
+import {printData} from "./graph.js";
 
 const PROXIE_URL = 'https://intense-mesa-62220.herokuapp.com/'
 const API1_URL = 'https://restcountries.herokuapp.com/api/v1/region/'
 const API2_URL = 'https://corona-api.com/countries/'
 
 let regionsList = []
-let dataCollected = []
+export let dataCollected = []
 
 async function getContriesFromRegion(region) {
     try {
         const data = await axios.get(PROXIE_URL+API1_URL+region,[
-            {
-                headers:"Access-Control-Allow-Origin",
-                Accept: "application/json",
-            }
+            // {
+            //     headers:"Access-Control-Allow-Origin",
+            //     Accept: "application/json",
+            // }
         ]);
         // arrangeCountries(data);
+        // console.log('ggggggg',data)
         return data
     }
     catch(error) {
@@ -25,30 +27,33 @@ async function getContriesFromRegion(region) {
 function arrangeCountries(data) {
     let listOfCountries = []
     data.data.forEach(countrie => {
+
         // console.log(countrie.name.common, countrie.cca2)
-        listOfCountries.push(countrie.cca2)
+        if(countrie.cca2!=='XK') listOfCountries.push(countrie.cca2)
     })
     return listOfCountries
     // listOfCountries = await Promise.all(listOfCountries)
     // getCovidDataCountriesPerRegion(listOfCountries)}
 }
 async function getCovidDataCountriesPerRegion(region) {
-        let result = []
-        region.forEach(countrie=>{
-        result.push(getCovidDataPerCounrie(countrie))
-    })
-        result = await Promise.all(result)
-        regionsList= result
-       console.log('redult', typeof result)
-        return result
 
-    // const countrieData =  await  getCovidDataPerCounrie('AF')
-    // console.log('result',result[0])
+        let result = []
+        region.forEach(countrie => {
+            result.push(getCovidDataPerCounrie(countrie))
+        })
+        result = await Promise.all(result)
+
+        regionsList = result
+        // console.log('redult', typeof result)
+        return result
+        // const countrieData =  await  getCovidDataPerCounrie('AF')
+        // console.log('result',result[0])
 
 }
+
 async function getCovidDataPerCounrie(countrie) {
     try {
-        const data = await axios.get(PROXIE_URL + API2_URL + countrie);
+        const data =  await axios.get(PROXIE_URL + API2_URL + countrie);
         // console.log('dddd', data.data.data)
         // arrangeData(data.data.data)
         return data
@@ -58,15 +63,30 @@ async function getCovidDataPerCounrie(countrie) {
 }
 
 function arrangeData(data){
-    regionsList.push(data)
+    dataCollected= data
     // console.log('datatada',data)
 
 }
 
-getContriesFromRegion('asia')
-    .then(data=>arrangeCountries(data))
-    .then(listOfCountries=>getCovidDataCountriesPerRegion(listOfCountries))
-    .then(result=>arrangeData(result)).then(()=>{
-    console.log('test',regionsList[0])
-    console.log('ee', regionsList[1].data.data.latest_data)
-})
+// getContriesFromRegion(region)
+//     .then(data=>arrangeCountries(data))
+//     .then(listOfCountries=>getCovidDataCountriesPerRegion(listOfCountries))
+//     .then(result=>arrangeData(result)).then(()=>{
+//     console.log('test',regionsList[0])
+//     console.log('ee', regionsList[1].data.data.latest_data)
+// })
+
+const regionsButtons = document.querySelector('.regions-buttons-div')
+
+regionsButtons.addEventListener('click',(event)=> {
+        let region = event.target.dataset.region
+        getContriesFromRegion(region)
+            .then(data => arrangeCountries(data))
+            .then(listOfCountries => getCovidDataCountriesPerRegion(listOfCountries))
+            .then(result => arrangeData(result)).then(() => {
+             printData(dataCollected)
+
+            // console.log('ee', regionsList[1].data.data.latest_data)
+        })
+    }
+)
