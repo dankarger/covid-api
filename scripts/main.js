7d4aefd0961e1bc4eb158cb2818c32f6eb0927e7
dankarger <karger.dan@gmail.com>
2021-12-09 14:03:07 +0200

    working api countrie and api corona 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
-console.log('hi')
+
 const PROXIE_URL = 'https://intense-mesa-62220.herokuapp.com/'
 const API1_URL = 'https://restcountries.herokuapp.com/api/v1/region/'
 const API2_URL = 'https://corona-api.com/countries/'
+
+const regionsList = []
 
 async function getContriesFromRegion(region) {
     try {
@@ -11,49 +13,49 @@ async function getContriesFromRegion(region) {
                 Accept: "application/json",
             }
         ]);
-
         arrangeCountries(data);
     }
     catch(error) {
         console.log(error);
     }
-
 }
-function arrangeCountries(data) {
-    const listOfCountries = []
-    data.data.forEach(countrie=>{
-        console.log(countrie.name.common,countrie.cca2)
+
+async function arrangeCountries(data) {
+    let listOfCountries = []
+    data.data.forEach(countrie => {
+        console.log(countrie.name.common, countrie.cca2)
         listOfCountries.push(countrie.cca2)
     })
-    getCovidDataCountriesPerRegion(listOfCountries)}
-
-
+    listOfCountries = await Promise.all(listOfCountries)
+    // getCovidDataCountriesPerRegion(listOfCountries)}
+}
 async function getCovidDataCountriesPerRegion(region) {
-    const countriesData = []
-
-    await region.forEach(countrie=>{
-        const data =  getCovidDataPerCounrie(countrie)
-        countriesData.push(data)
+       const result = region.forEach(countrie=>{
+        getCovidDataPerCounrie(countrie)
     })
+    return result
+    // printData(result)
     // const countrieData =  await  getCovidDataPerCounrie('AF')
-    console.log(countriesData)
-    console.log('d',countriesData)
+    // console.log('result',result[0])
+
 }
 async function getCovidDataPerCounrie(countrie) {
     try {
-        const data = await axios.get(PROXIE_URL+API2_URL+countrie,[
-            {
-                headers:"Access-Control-Allow-Origin",
-                Accept: "application/json",
-                // Content-Type: "application/json",
-            }
-        ]);
+        const data = await axios.get(PROXIE_URL + API2_URL + countrie);
+        console.log('dddd', data.data.data)
+        arrangeData(data.data.data)
         return data
-    }
-    catch(error) {
+    } catch (error) {
         console.log(error);
     }
-
-
 }
+
+function arrangeData(data){
+    regionsList.push(data)
+    // console.log('data',data)
+}
+
 getContriesFromRegion('asia')
+    .then(data=>arrangeData(data))
+    .then(listOfCountries=>getCovidDataCountriesPerRegion(listOfCountries))
+    .then()
